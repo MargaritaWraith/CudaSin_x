19b8ac870dab8d1246bf1c2cb17640badf30d150
Kondr <Kondr@ANDROMEDA>
2019-03-04 19:49:00 +0300

    Добавьте файлы проекта.

---
+++ CudaTest2/kernel.cu
@@ -0,0 +1,102 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <Math.h>
+#include <stdio.h>
+
+cudaError_t addWithCuda(double *sinx, unsigned int size);
+
+__global__ void addKernel(double *sinx)
+{
+    //int i = threadIdx.x;
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+	sinx[i] = sin(i*3.14 / 1024);
+
+}
+
+int main()
+{
+    const int arraySize = 1024;
+    double sinx[arraySize] = { 0 };
+
+    // Add vectors in parallel.
+    cudaError_t cudaStatus = addWithCuda(sinx, arraySize);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addWithCuda failed!");
+        return 1;
+    }
+
+	printf("sin[0;pi) with 1024 steps\n\n");
+
+
+	for (int i = 0; i < arraySize; i++)
+	{
+		printf("sin[%d] = %f\n", i, sinx[i]);
+	}
+
+
+    // cudaDeviceReset must be called before exiting in order for profiling and
+    // tracing tools such as Nsight and Visual Profiler to show complete traces.
+    cudaStatus = cudaDeviceReset();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceReset failed!");
+        return 1;
+    }
+
+    return 0;
+}
+
+// Helper function for using CUDA to add vectors in parallel.
+cudaError_t addWithCuda(double *sinx, unsigned int size)
+{
+    double *dev_sinx = 0;
+ 
+    cudaError_t cudaStatus;
+
+    // Choose which GPU to run on, change this on a multi-GPU system.
+    cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+        goto Error;
+    }
+
+    // Allocate GPU buffers for three vectors (two input, one output)    .
+    cudaStatus = cudaMalloc((void**)&dev_sinx, size * sizeof(double));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+
+
+    // Launch a kernel on the GPU with one thread for each element.
+
+    addKernel<<<4, 256>>>(dev_sinx);
+
+    // Check for any errors launching the kernel
+    cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        goto Error;
+    }
+    
+    // cudaDeviceSynchronize waits for the kernel to finish, and returns
+    // any errors encountered during the launch.
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+        goto Error;
+    }
+
+    // Copy output vector from GPU buffer to host memory.
+    cudaStatus = cudaMemcpy(sinx, dev_sinx, size * sizeof(double), cudaMemcpyDeviceToHost);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+Error:
+    cudaFree(dev_sinx);
+    
+    return cudaStatus;
+}
